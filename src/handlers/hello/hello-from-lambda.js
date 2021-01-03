a62f01a695fae59d75d6ed8588e6db325c5dfe4e
hbirkdale <hank.birkdale@gmail.com>
2021-01-03 13:50:58 -0500

    First real test of state machine

--- src/handlers/hello/hello-from-lambda.js
+++ src/handlers/hello/hello-from-lambda.js
@@ -3,7 +3,7 @@
  */
 exports.helloFromLambdaHandler = async () => {
     // If you change this message, you will need to change hello-from-lambda.test.js
-    const message = 'Hello from Lambda-calling from statemachine!';
+    const message = 'Hello from Lambda-calling from lambda!';
 
     // All log statements are written to CloudWatch
     console.info(`${message}`);
